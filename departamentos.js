18121975660ae097f63985353abdea52552f08ca
DanielRincon15 <inglopezrincon@gmail.com>
2021-09-27 16:18:41 -0500

    Ejercicio Departamentos

---
+++ departamentos.js
@@ -0,0 +1,136 @@
+const departamento =() => {
+
+let nombreDepartamento = prompt ("Ingrese el departamento");
+
+switch (nombreDepartamento) {
+
+    case "Amazonas":
+        alert("Su capital es: Leticia");
+        break;
+    
+    case "Antioquia":
+        alert("Su capital es: Medellin");
+        break;
+
+    case "Arauca":
+        alert("Su capital es: Arauca");
+        break;
+
+    case "Atlantico":
+        alert("Su capital es: Barranquilla");
+        break;
+
+    case "Bolivar":
+        alert("Su capital es: Cartagena de Indias");
+        break;
+            
+    case "Boyaca":
+        alert("Su capital es: Tunja");
+        break;
+        
+    case "Caldas":
+        alert("Su capital es: Manizales");
+        break;
+        
+    case "Caqueta":
+        alert("Su capital es: Florencia");
+        break;
+        
+    case "Casanare":
+        alert("Su capital es: Yopal");
+        break;
+        
+    case "Cauca":
+        alert("Su capital es: Popayan");
+        break;
+    
+    case "Cesar":
+        alert("Su capital es: Valledupar");
+        break;
+    
+     case "Choco":
+        alert("Su capital es: Quibdo");
+        break;
+    
+    case "Cordoba":
+        alert("Su capital es: Monteria");
+        break;
+                
+    case "Cundinamarca":
+        alert("Su capital es: Bogota D.C.");
+        break;
+            
+    case "Guainia":
+        alert("Su capital es: Inirida");
+        break;
+            
+    case "Guaviare":
+        alert("Su capital es: San Jose del Guaviare");
+        break;
+
+    case "Huila":
+        alert("Su capital es: Neiva");
+        break;
+    
+    case "Guajira":
+        alert("Su capital es: Rioacha");
+        break;
+
+    case "Magdalena":
+        alert("Su capital es: Santa Marta");
+        break;
+
+    case "Meta":
+        alert("Su capital es: Villavicencio");
+        break;
+
+    case "Nariño":
+        alert("Su capital es: Pasto");
+        break;
+            
+    case "Norte de Santander":
+        alert("Su capital es: Cucuta");
+        break;
+        
+    case "Putumayo":
+        alert("Su capital es: Mocoa");
+        break;
+        
+    case "Quindio":
+        alert("Su capital es: Armenia");
+        break;
+        
+    case "Risaralda":
+        alert("Su capital es: Pereira");
+        break;
+        
+    case "San Andres y Providencia":
+        alert("Su capital es: San Andres");
+        break;
+    
+    case "Santander":
+        alert("Su capital es: Bucaramanga");
+        break;
+    
+     case "Sucre":
+        alert("Su capital es: Sincelejo");
+        break;
+    
+    case "Tolima":
+        alert("Su capital es: Ibague");
+        break;
+                
+    case "Valle del Cauca":
+        alert("Su capital es: Cali");
+        break;
+            
+    case "Vaupes":
+        alert("Su capital es: Mitu");
+        break;
+            
+    case "Vichada":
+        alert("Su capital es: Puerto Carreño");
+        break;
+        }
+
+}
